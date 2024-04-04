eb4060e113afb9e05ef3a7b2d2b90a3456bf12f7
shuvo794 <shuvohossain794@gmail.com>
2024-04-04 18:01:58 +0600

    work a footer section

--- app/(Home)/footer.tsx
+++ app/(Home)/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="hidden lg:block h-20 border-t-2 border-slate-200 p-2">
       <div className="max-w-screen mx-auto flex items-center justify-evenly">
         <Button size="lg" variant="ghost">
-          Italy
+          © 2024 LMS. Built with Next.js.
         </Button>
       </div>
     </div>
