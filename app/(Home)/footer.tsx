94f55486e6d18dbf93d0d155b7e180b4b6ff3045
shuvo794 <shuvohossain794@gmail.com>
2024-04-04 17:47:44 +0600

    work a footer section

--- app/(Home)/footer.tsx
+++ app/(Home)/footer.tsx
@@ -6,53 +6,6 @@ const Footer = () => {
     <div className="hidden lg:block h-20 border-t-2 border-slate-200 p-2">
       <div className="max-w-screen mx-auto flex items-center justify-evenly">
         <Button size="lg" variant="ghost">
-          <Image
-            className="mr-4 rounded-md"
-            width={32}
-            height={40}
-            src="/hr.svg"
-            alt="Croatain"
-          />
-          Croatain
-        </Button>
-        <Button size="lg" variant="ghost">
-          <Image
-            className="mr-4 rounded-md"
-            width={32}
-            height={40}
-            src="/es.svg"
-            alt="spanish "
-          />
-          Spanish
-        </Button>
-        <Button size="lg" variant="ghost">
-          <Image
-            className="mr-4 rounded-md"
-            width={32}
-            height={40}
-            src="/jp.svg"
-            alt="Japan"
-          />
-          Japan
-        </Button>
-        <Button size="lg" variant="ghost">
-          <Image
-            className="mr-4 rounded-md"
-            width={32}
-            height={40}
-            src="/fr.svg"
-            alt="France"
-          />
-          France
-        </Button>
-        <Button size="lg" variant="ghost">
-          <Image
-            className="mr-4 rounded-md"
-            width={32}
-            height={40}
-            src="/it.svg"
-            alt="italy"
-          />
           Italy
         </Button>
       </div>
