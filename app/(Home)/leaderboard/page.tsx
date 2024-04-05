a9723ec4cb3d7c1c9bba120659ac4afadfaf85aa
shuvo794 <shuvohossain794@gmail.com>
2024-04-05 17:25:43 +0600

    send

---
+++ app/(Home)/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-neutral-700 ml-3 text-center animate-pulse mt-[150px]">
+        Leaderboard Coming Soon
+      </h1>
+    </div>
+  );
+};
+
+export default page;
