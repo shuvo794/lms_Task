be39b7a9c10d880e336e7c32bb37c8e10002d206
shuvo794 <shuvohossain794@gmail.com>
2024-04-05 17:32:56 +0600

    send

---
+++ app/(Home)/shop/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-5xl font-bold text-green-700 ml-3 text-center animate-pulse mt-[150px]">
+        shop coming soon
+      </h2>
+    </div>
+  );
+};
+
+export default page;
