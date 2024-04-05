b3481c14b77b8f6a307c9d8fc760129e53dddb6b
shuvo794 <shuvohossain794@gmail.com>
2024-04-05 17:30:36 +0600

    send

---
+++ app/(Home)/Quests/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-5xl font-bold text-green-700 ml-3 text-center animate-pulse mt-[150px]">
+        Quests Coming Soon
+      </h2>
+    </div>
+  );
+};
+
+export default page;
--- app/(Home)/leaderboard/page.tsx
+++ app/(Home)/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-5xl font-bold text-neutral-700 ml-3 text-center animate-pulse mt-[150px]">
+      <h1 className="text-5xl font-bold text-green-700 ml-3 text-center animate-pulse mt-[150px]">
         Leaderboard Coming Soon
       </h1>
     </div>
